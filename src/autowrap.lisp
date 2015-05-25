41eb99b7f0c789c1c4ac55a8f8c8233cc9360d18
Ole Rohne <ole.rohne@gmail.com>
2015-05-25 15:49:55 +0200

    Setup to run c2ffi/autowrap on Darwin

--- src/autowrap.lisp
+++ src/autowrap.lisp
@@ -5,12 +5,29 @@
   :accessor-package :sdl2-ffi.accessors
   :function-package :sdl2-ffi.functions
   :spec-path '(sdl2 autowrap-spec)
+  ;; gcc system defines: echo | gcc -dM -E -
+  ;; gcc system includes: echo | gcc -xc -E -v -
+  :sysincludes 
+  (cl:list 
+   "/usr/local/include"
+   "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/6.1.0/include"
+   "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
+   "/usr/include")
+  :exclude-arch ("i686-pc-linux-gnu"
+		 "x86_64-pc-linux-gnu"
+		 "i686-pc-windows-msvc"
+		 "x86_64-pc-windows-msvc"
+		 "i686-apple-darwin9"
+		 ;; :exclude-arch "x86_64-apple-darwin9"
+		 "i386-unknown-freebsd"
+		 "x86_64-unknown-freebsd")
   :exclude-sources ("/usr/local/lib/clang/([^/]*)/include/(?!stddef.h)"
                     "/usr/include/"
                     "/usr/include/arm-linux-gnueabihf")
   :include-sources ("stdint.h"
                     "bits/types.h"
                     "sys/types.h"
+		    "_types/"
                     "SDL2")
   :exclude-definitions ("SDL_LogMessageV"
                         "SDL_vsnprintf"
