56bae9bfbf41158a753ca3c1318d5949fde28433
Ryan Pavlik <rpavlik@gmail.com>
2016-03-01 11:17:52 -0600

    Add GET-WINDOW-WM-INFO and necessary header etc modifications (#65)

--- src/autowrap.lisp
+++ src/autowrap.lisp
@@ -23,7 +23,8 @@
 		 "x86_64-unknown-freebsd")
   :exclude-sources ("/usr/local/lib/clang/([^/]*)/include/(?!stddef.h)"
                     "/usr/include/"
-                    "/usr/include/arm-linux-gnueabihf")
+                    "/usr/include/arm-linux-gnueabihf"
+                    "/usr/include/X11/")
   :include-sources ("stdint.h"
                     "bits/types.h"
                     "sys/types.h"
@@ -33,6 +34,7 @@
                         "SDL_vsnprintf"
                         "_inline$"
                         "^_mm_")
+  :include-definitions ("^XID$" "^Window$" "^Display$" "^_XDisplay$")
   :symbol-exceptions (("SDL_Log" . "SDL-LOGGER")
                       ("SDL_log" . "SDL-LOGN")
                       ("SDL_RWops" . "SDL-RWOPS")
@@ -40,8 +42,9 @@
                       ("SDL_GLattr" . "SDL-GLATTR")
                       ("SDL_GLprofile" . "SDL-GLPROFILE")
                       ("SDL_GLcontextFlag" . "SDL-GLCONTEXT-FLAG")
+                      ("SDL_SysWMinfo" . "SDL-SYSWM-INFO")
+                      ("SDL_SysWMmsg" . "SDL-SYSWM-MSG")
                       ("SDL_TRUE" . "TRUE")
                       ("SDL_FALSE" . "FALSE"))
   :no-accessors cl:t
   :release-p cl:t)
-
